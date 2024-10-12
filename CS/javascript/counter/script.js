3c71039bbb8f746370946180c2d71228e4a4a53e
acbboyzZ <asbrown1410@icloud.com>
2024-10-12 11:10:16 -0400

    Update script.js
    changes

--- CS/javascript/counter/script.js
+++ CS/javascript/counter/script.js
@@ -1,4 +1,4 @@
 function counter() {
-counter_number = document.getElementById('counter_number);
+counter_number = document.getElementById('counter_number');
   counter_number.innerHTML=parseInt(counter_number.innerHTML)+1;
 }
